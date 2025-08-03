e260b4acb8ef287a68a059785a448c05355d35cd
nguyenduc11 <nguyenduc11@gmail.com>
2025-08-03 16:12:36 +0700

    modify navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Image from 'next/image'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <nav>
+            <div className="container mx-auto px-3 md:px-12">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <Image src={assets.logo} 
+                        className="w-28 cursor-pointer" 
+                        alt="" />
+                    </div>
+                    <div>
+                        <ul className="flex space-x-6">
+                            <li>
+                                <a href="#" className="text-[#111]">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-[#111]">About</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-[#111]">Services</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-[#111]">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-[#111]">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="bg-[#111] text-white px-6 py-2 rounded-md">Hire me</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-
+import Navbar from "./components/Navbar";
+import { assets } from "../assets/assets";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <Navbar />
+    </>
   );
 }
