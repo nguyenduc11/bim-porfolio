a79b1cef7f441b032230595fd232b41afcc3d643
nguyenduc11 <nguyenduc11@gmail.com>
2025-08-24 23:51:43 +0700

    feat: add root layout with Vercel Analytics and custom fonts setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
